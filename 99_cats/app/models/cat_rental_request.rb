284bd42fb1e2f2c135f1321250e1e3a5640f0ab9
AAlfarho <st.alfaro@gmail.com>
2017-08-22 23:39:46 -0700

    implement overlappin requests where active record

--- 99_cats/app/models/cat_rental_request.rb
+++ 99_cats/app/models/cat_rental_request.rb
@@ -13,7 +13,7 @@
 
 class CatRentalRequest < ApplicationRecord
 
-  STATUS = [ 'PENDING', 'APPROVED', 'DENIED']
+  STATUS = [ 'PENDING', 'APPROVED', 'DENIED'].freeze
 
   validates :status, inclusion: { in: STATUS }
   validates :cat_id, :start_date, :end_date, presence: true
@@ -23,8 +23,12 @@ class CatRentalRequest < ApplicationRecord
     foreign_key: :cat_id,
     class_name: :Cat
 
-    def overlapping_requests
-
-    end
+  def overlapping_requests
+    CatRentalRequest
+      .where.not(id: self.id)
+      .where(cat_id: cat_id)
+      .where.not('start_date > :end_date OR end_date < :start_date',
+                  start_date: start_date, end_date: end_date)
+  end
 
 end
